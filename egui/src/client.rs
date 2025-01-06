501c3f24c43afce8a46d0ceebac80046884f6cbd
Bryce Mecum <petridish@gmail.com>
2025-01-05 21:28:08 -0800

    working version

---
+++ egui/src/client.rs
@@ -0,0 +1,20 @@
+use tarpc::{client, context, tokio_serde::formats::Json};
+
+#[tarpc::service]
+pub trait World {
+    async fn hello(name: String) -> String;
+    async fn handle_recvfrom(data: Vec<u8>) -> String;
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let transport = tarpc::serde_transport::tcp::connect("[::1]:5000", Json::default);
+
+    let client = WorldClient::new(client::Config::default(), transport.await?).spawn();
+
+    let _hello = client.hello(context::current(), "Stim".to_string()).await?;
+    let data = vec![0x60, 0x61, 0x62];
+    let _hello = client.handle_recvfrom(context::current(), data).await?;
+
+    Ok(())
+}
